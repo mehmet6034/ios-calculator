14790b2acf1b1b3b6d2b434b01818be2e644a46d
Mehmet Yanar <ynr.mehmet.60@gmail.com>
2022-08-30 17:39:44 +0200

    work on calculator.

---
+++ app.js
@@ -0,0 +1,9 @@
+//*0-9 tus yazdirma
+
+// let seven = document.getElementById("two")
+document.querySelector(".two-one").addEventListener("click", addNumber);
+
+
+function addNumber(){
+    document.getElementById(`screen2`).innerHTML += 7
+}   
