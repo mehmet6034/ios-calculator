687353fd16915bfda7cd63625de9889328b1df8b
Mehmet Yanar <ynr.mehmet.60@gmail.com>
2022-09-27 15:50:50 +0200

    begining of js code

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-//*0-9 tus yazdirma
+//! To find element with DOM
+let screen1 = document.querySelector(`#screen1`);
+let screen2 = document.querySelector(`#screen2`);
+let playground = document.querySelector(`.playground`);
 
-// let seven = document.getElementById("two")
-document.querySelector(".two-one").addEventListener("click", addNumber);
 
-
-function addNumber(){
-    document.getElementById(`screen2`).innerHTML += 7
-}   
+playground.addEventListener(`click`, (e) =>{
+//? we use contains except includes to search whether list contain elements or not
+    if(e.target.classList.contains(`num`)){
+        screen2.innerHTML += e.target.value;
+    } else if(e.target.classList.contains(`operator`)){
+        if()
+        screen1 = screen2.innerHTML
+        screen2 = ``
+        switch (e.target.value) {
+            case `+`:
+                
+                break;
+        
+            default:
+                break;
+        }
+        console.log(e.target.value);
+    }
+   
+})
